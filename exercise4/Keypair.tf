4153ab9ab5def1042da4b74c4c699afe0df0770d
Manas <73131991+manas2402@users.noreply.github.com>
2026-05-03 14:55:56 +0530

    Update Keypair.tf

--- exercise4/Keypair.tf
+++ exercise4/Keypair.tf
@@ -1,9 +1,9 @@
 resource "aws_key_pair" "dove-key" {
   key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBfHOL5d2QQ1Z/WhFK5wmfyqSOIlYfPNrevfFPBViBCi PC-DELL@DESKTOP-JFNEIHB"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1N*******************************************************"
 }
 
 resource "aws_key_pair" "test-key" {
   key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBegtAI02rQ92sZ3JvVxAKNLq8/H7q2ccF0gMNDD8jU PC-DELL@DESKTOP-JFNEIHB"
+  public_key = "ssh-ed25519 AAAAC3N****************************************"
 }
