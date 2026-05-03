11814f85345e11502144e32fab0ec3117e1b4faf
Manas <73131991+manas2402@users.noreply.github.com>
2026-05-03 14:55:25 +0530

    Update public keys in Keypair.tf
    Redacted public keys for security reasons.

--- exercise2/Keypair.tf
+++ exercise2/Keypair.tf
@@ -1,9 +1,9 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBfHOL5d2QQ1Z/WhFK5wmfyqSOIlYfPNrevfFPBViBCi PC-DELL@DESKTOP-JFNEIHB"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NT*****************************************"
 }
 
 resource "aws_key_pair" "test-key" {
   key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBegtAI02rQ92sZ3JvVxAKNLq8/H7q2ccF0gMNDD8jU PC-DELL@DESKTOP-JFNEIHB"
+  public_key = "ssh-ed25519 AAAAC3Nza***************************************"
 }
